f779b7b8a0d92fb36062190db781f2bc013f5c19
Bent Tranberg <bent.tranberg@gmail.com>
2021-02-27 03:11:45 +0100

    Publish initial test.

---
+++ zoom_index.js
@@ -0,0 +1,111 @@
+dictwords = ["chapter 0 28 148 2 30 136 3 16 136 5 40 152",
+	"click 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 10 16",
+	"display 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 10 16",
+	"table 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 10 16",
+	"contents 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 10 16",
+	"navigation 0 10 8 1 10 8 2 10 8 3 10 8 4 10 8 5 10 8 6 10 8",
+	"topics 0 10 8 1 10 8",
+	"above 0 10 8 1 10 8",
+	"this 0 10 8 1 10 8",
+	"level 0 10 8 1 10 8",
+	"enter 0 15 66 2 15 72 3 15 72 4 15 72 5 15 72 6 15 72",
+	"topic 0 15 66 2 39 232 3 15 72 4 39 216 5 15 72 6 39 216",
+	"text 0 15 66 2 15 72 3 15 72 4 15 72 5 15 72 6 15 72",
+	"here 0 15 66 2 15 72 3 15 72 4 15 72 5 15 72 6 15 72",
+	"chapter-2.html 0 6 64",
+	"introduction 1 28 148 4 16 136 6 16 136",
+	"why 1 15 66",
+	"frostbits 1 15 66",
+	"google 1 15 66",
+	"search 1 15 66",
+	"gives 1 15 66",
+	"hits 1 15 66",
+	"introduction.html 1 6 64",
+	"new 2 28 168",
+	"sub 2 16 136 5 28 168",
+	"2.1 2 16 136 5 28 152",
+	"new-topic.html 2 6 64",
+	"overview 3 28 152",
+	"overview.html 3 6 64",
+	"second 4 28 152",
+	"second-topic.html 4 6 64",
+	"sub-chapter-2_1.html 5 6 64",
+	"welcome 6 28 152",
+	"welcome-topic.html 6 6 64"];
+skipwords = ["and",
+	"or",
+	"the",
+	"it",
+	"is",
+	"an",
+	"on",
+	"we",
+	"us",
+	"to",
+	"of",
+	"has",
+	"be",
+	"all",
+	"for",
+	"in",
+	"as",
+	"so",
+	"are",
+	"that",
+	"can",
+	"you",
+	"at",
+	"its",
+	"by",
+	"have",
+	"with",
+	"into"];
+var STR_FORM_SEARCHFOR = "Search for:";
+var STR_FORM_SUBMIT_BUTTON = "Submit";
+var STR_FORM_RESULTS_PER_PAGE = "Results per page:";
+var STR_FORM_MATCH = "Match:";
+var STR_FORM_ANY_SEARCH_WORDS = "any search words";
+var STR_FORM_ALL_SEARCH_WORDS = "all search words";
+var STR_NO_QUERY = "No search query entered.";
+var STR_RESULTS_FOR = "Search results for:";
+var STR_NO_RESULTS = "No results";
+var STR_RESULT = "result";
+var STR_RESULTS = "results";
+var STR_PHRASE_CONTAINS_COMMON_WORDS = "Your search query contained too many common words to return the entire set of results available. Please try again with a more specific query for better results.";
+var STR_SKIPPED_FOLLOWING_WORDS = "The following word(s) are in the skip word list and have been omitted from your search:";
+var STR_SKIPPED_PHRASE = "Note that you can not search for exact phrases beginning with a skipped word";
+var STR_SUMMARY_NO_RESULTS_FOUND = "No results found.";
+var STR_SUMMARY_FOUND_CONTAINING_ALL_TERMS = "found containing all search terms.";
+var STR_SUMMARY_FOUND_CONTAINING_SOME_TERMS = "found containing some search terms.";
+var STR_SUMMARY_FOUND = "found.";
+var STR_PAGES_OF_RESULTS = "pages of results.";
+var STR_POSSIBLY_GET_MORE_RESULTS = "You can possibly get more results searching for";
+var STR_ANY_OF_TERMS = "any of the terms";
+var STR_DIDYOUMEAN = "Did you mean:";
+var STR_SORTEDBY_RELEVANCE = "Sorted by relevance";
+var STR_SORTBY_RELEVANCE = "Sort by relevance";
+var STR_SORTBY_DATE = "Sort by date";
+var STR_SORTEDBY_DATE = "Sorted by date &#9660;";
+var STR_RESULT_TERMS_MATCHED = "Terms matched: ";
+var STR_RESULT_SCORE = "Score: ";
+var STR_RESULT_URL = "URL:";
+var STR_RESULT_PAGES = "Result Pages:";
+var STR_RESULT_PAGES_PREVIOUS = "Previous";
+var STR_RESULT_PAGES_NEXT = "Next";
+var STR_FORM_CATEGORY = "Category:";
+var STR_FORM_CATEGORY_ALL = "All";
+var STR_FORM_DATE_FROM = "From:";
+var STR_FORM_DATE_TO = "To:";
+var STR_FORM_DATE_BUTTON = "&laquo;";
+var STR_RESULTS_IN_ALL_CATEGORIES = "in all categories";
+var STR_RESULTS_IN_CATEGORY = "in category";
+var STR_POWEREDBY = "Search powered by";
+var STR_MORETHAN = "More than";
+var STR_ALL_CATS = "all categories";
+var STR_CAT_SUMMARY = "Refine your search by category:";
+var STR_OR = "or";
+var STR_RECOMMENDED = "Recommended links";
+var STR_SORTEDBY_DATE_ASC = "Sorted by date &#9650;";
+var STR_SEARCH_TOOK = "Search took";
+var STR_SECONDS = "seconds";
+var STR_MAX_RESULTS = "You have requested more results than served per query. Please try again with a more precise query.";
---
+++ zoom_pageinfo.js
@@ -0,0 +1,14 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["./chapter-2.html","Chapter 2","Enter topic text here.",""],
+["./introduction.html","Introduction","Why Frostbits? A Google search gives no hits.",""],
+["./new-topic.html","Chapter 2 &#62; Sub chapter 2.1 &#62; New topic","Enter topic text here.",""],
+["./overview.html","Chapter 2 &#62; Overview","Enter topic text here.",""],
+["./second-topic.html","Introduction &#62; Second topic","Enter topic text here.",""],
+["./sub-chapter-2_1.html","Chapter 2 &#62; Sub chapter 2.1","Enter topic text here.",""],
+["./welcome-topic.html","Introduction &#62; Welcome topic","Enter topic text here.",""]];
